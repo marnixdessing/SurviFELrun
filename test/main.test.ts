0f9a4e56de3245106feed71fd78bdb088e830af6
Marnix Dessing <m.dessing@nijmegen.nl>
2025-03-01 17:29:22 +0100

    chore: project created with projen

---
+++ test/main.test.ts
@@ -0,0 +1,4 @@
+
+test('Snapshot', () => {
+  console.log('no tests');
+});
---
+++ website/upload.sh
@@ -0,0 +1,4 @@
+export AWS_PROFILE=personal
+aws s3 cp /Users/marnix/Development/Personal/SurviFELrun/website/style.css s3://survifelrun-website/style.css
+aws s3 cp /Users/marnix/Development/Personal/SurviFELrun/website/index.html s3://survifelrun-website/index.html
+aws s3 cp /Users/marnix/Development/Personal/SurviFELrun/website/fullscreenmenu.css s3://survifelrun-website/fullscreenmenu.css
